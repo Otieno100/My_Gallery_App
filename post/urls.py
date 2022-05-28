84b2c4a2c541e499440b740a0d96c06122a35b97
Otieno100 <brian.masafu@student.moringaschool.com>
2022-05-28 21:41:40 +0300

    updated url for search form

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     # re_path('^$',views.index,name = 'welcome'),
 
     re_path(r'^$',views.post_of_day,name='postToday'),
+    re_path(r'^search/', views.search_results, name='search_results')
 
 ]
