2fbba156c0249eaabcf1dd4bdb98b05fa78a6db0
Otieno100 <brian.masafu@student.moringaschool.com>
2022-05-28 20:37:11 +0300

    created models for pub_date

--- post/models.py
+++ post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 
 
@@ -9,3 +10,61 @@ class User(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+      return self.first_name
+
+    def save_user(self):
+        self.save()
+
+
+
+
+class Location(models.Model):
+    """
+    This class provies the location of the image where it was taken
+    """
+    city = models.CharField(max_length=244)
+    country = models.CharField(max_length=244)
+
+
+
+
+class Category(models.Model):
+    """
+    A class for the category the Images falls under
+    """
+    name = models.CharField(max_length=144)
+
+
+
+
+
+
+
+class Images(models.Model):
+    """
+    A class thaat determines how photos will be saved into the database
+    """
+    name = models.CharField(max_length=344)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    #  pub_date = models.DateTimeField(auto_now_add=True)
+    # image = models.ImageField(blank=True)
+    def __str__(self):
+        return self.title
+
+
+    @classmethod
+    def todays_post(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news        
