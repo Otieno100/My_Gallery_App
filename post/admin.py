6c203f5a87e7acd729b4003512365ac7ab3e096c
Otieno100 <brian.masafu@student.moringaschool.com>
2022-05-28 20:42:03 +0300

    added untracked files from random imports and dependencies

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Images, User, Category, Location
+
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('categorys',)
+admin.site.register(User)
+admin.site.register(Images)
+admin.site.register(Location)
+admin.site.register(Category)
--- post/urls.py
+++ post/urls.py
@@ -2,5 +2,8 @@ from django.urls import re_path,include
 from . import views
 
 urlpatterns=[
-    re_path('^$',views.index,name = 'welcome'),
+    # re_path('^$',views.index,name = 'welcome'),
+
+    re_path(r'^$',views.post_of_day,name='postToday'),
+
 ]
