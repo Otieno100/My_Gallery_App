b7921a3b9d590eb78e771d38802df751846c3141
Otieno100 <brian.masafu@student.moringaschool.com>
2022-05-29 13:10:24 +0300

    updated views for location function

--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
-from .models import  Images
+from .models import  Images,Location
 
 # Create your views here.
 def index(request):
@@ -19,7 +19,7 @@ def search_results(request):
 
     if 'images' in request.GET and request.GET["images"]:
         search_term = request.GET.get("images")
-        searched_images = Images.search_by_title(search_term)
+        searched_images = Images.search_by_name(search_term)
         message = f"{search_term}"
 
         return render(request, 'Gallery-images/search.html',{"message":message,"images": searched_images})
@@ -30,9 +30,14 @@ def search_results(request):
 
 
 
-
-# def locate_image(request,location):
-#     photos = Image.filter_by_location(location)
-#     return render(request,'album/location.html',{"located_images":photos})
-
     
+def location_filter(request, id):
+    images = Images.objects.filter(location__id = id)
+    results = len(images)
+    location = Location.objects.get(id = id)
+    locations = Location.objects.all()
+
+    return render(request, "gallery/location.html", context={"images":images,"results":results,"location":location,"locations":locations})
+                                                             
+                                                             
+                                                             
